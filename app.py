9b369b66158d2c0915824804d6cdf2424f2aca05
hee56 <145115966+hee56@users.noreply.github.com>
2025-06-01 16:10:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from dotenv import load_dotenv
 
 # 🔐 API 키 로딩
 load_dotenv()
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
+
 
 # 모델 준비
 model = genai.GenerativeModel("gemini-2.0-flash-exp")
